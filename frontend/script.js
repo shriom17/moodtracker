23a16ad691d5bfa1a6e5662eb4660929025162d7
shriom17 <129147531+shriom17@users.noreply.github.com>
2025-12-25 22:11:17 +0530

    URI updated to production server

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://moodtracker-1-3dmf.onrender.com';
 
 // Google Sign-In callback function
 function handleCredentialResponse(response) {
